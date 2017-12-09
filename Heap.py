91f41e546c078895df5388694e17d695546e952e
Marco Lamanda <32776739+LimonJuice@users.noreply.github.com>
2017-12-09 13:04:57 +0100

    Update Heap.py

--- Heap.py
+++ Heap.py
@@ -3,8 +3,6 @@ class  BinHeap:
         self.heapLst=[0]
         self.currentSize=0
 
-
-
     def percUp(self,i):
         while i//2>0:
             if self.heapLst[i]<self.heapLst[i//2]:
@@ -13,20 +11,39 @@ class  BinHeap:
                 temp=self.heapLst[i]
             i=i//2
 
-    def insert(self, x):
+    def insertion(self, x):
         self.heapLst.append(x)
         self.currentSize = self.currentSize + 1
         self.percUp(self.currentSize)
 
     def percDown(self, i):
         while (i * 2) <= self.currentSize:
-            mc = self.minChild(i)
-            if self.heapLst[i] > self.heapLst[mc]:
+            sm = self.minChild(i)
+            if self.heapLst[i] > self.heapLst[sm]:
                 temp = self.heapLst[i]
-                self.heapLst[i] = self.heapLst[mc]
-                self.heapLst[mc] = temp
-            i = mc
+                self.heapLst[i] = self.heapLst[sm]
+                self.heapLst[sm] = temp
+            i = sm
+            
+            
+    def minChild(self, i):
+           if i * 2 + 1 > self.currentSize:
+               return i * 2
+           else:
+               if self.heapList[i * 2] < self.heapList[i * 2 + 1]:
+                   return i * 2
+               else:
+                   return i * 2 + 1
 
+      def delMin(self):
+          retval = self.heapList[1]
+          self.heapLst[1] = self.heapLst[self.currentSize]
+          self.currentSize = self.currentSize - 1
+          self.heapLst.pop()
+          self.percDown(1)
+          return retval
+            
+            
     def maxChild(self, i):
         if i * 2 + 1 < self.currentSize:
             return i * 2
