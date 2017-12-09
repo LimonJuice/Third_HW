532c55a84ca5fa637b7561b7c1d6449663ad8885
Marco Lamanda <32776739+LimonJuice@users.noreply.github.com>
2017-12-09 16:24:31 +0100

    Update and rename Heap.py to Heap_Maximum.py

--- Heap_Maximum.py
+++ Heap_Maximum.py
@@ -1,4 +1,4 @@
-class  BinHeap:
+class  HeapMaximum:
     def __init__(self):
         self.heapLst=[0]
         self.currentSize=0
@@ -13,21 +13,21 @@ class  BinHeap:
 
     def insertion(self, x):
         self.heapLst.append(x)
-        self.currentSize = self.currentSize + 1
+        self.currentSize=self.currentSize + 1
         self.percUp(self.currentSize)
 
     def percDown(self, i):
-        while (i * 2) <= self.currentSize:
-            sm = self.minChild(i)
-            if self.heapLst[i] > self.heapLst[sm]:
-                temp = self.heapLst[i]
-                self.heapLst[i] = self.heapLst[sm]
-                self.heapLst[sm] = temp
-            i = sm
+        while (i * 2)<=self.currentSize:
+            sm=self.minChild(i)
+            if self.heapLst[i]>self.heapLst[sm]:
+                temp=self.heapLst[i]
+                self.heapLst[i]=self.heapLst[sm]
+                self.heapLst[sm]=temp
+            i=sm
             
             
     def minChild(self, i):
-           if i * 2 + 1 > self.currentSize:
+           if i * 2 + 1>self.currentSize:
                return i * 2
            else:
                if self.heapList[i * 2] < self.heapList[i * 2 + 1]:
@@ -36,35 +36,28 @@ class  BinHeap:
                    return i * 2 + 1
 
       def delMin(self):
-          retval = self.heapList[1]
-          self.heapLst[1] = self.heapLst[self.currentSize]
-          self.currentSize = self.currentSize - 1
+          retval=self.heapList[1]
+          self.heapLst[1]=self.heapLst[self.currentSize]
+          self.currentSize=self.currentSize - 1
           self.heapLst.pop()
           self.percDown(1)
           return retval
             
             
     def maxChild(self, i):
-        if i * 2 + 1 < self.currentSize:
+        if i * 2 + 1<self.currentSize:
             return i * 2
         else:
-            if self.heapLst[i * 2] > self.heapLst[i * 2 + 1]:
+            if self.heapLst[i * 2]>self.heapLst[i * 2 + 1]:
                 return i * 2
             else:
                 return i * 2 + 1
 
     def delMax(self):
-        retval = self.heapLst[1]
-        self.heapLst[1] = self.heapLst[self.currentSize]
-        self.currentSize = self.currentSize - 1
+        retval=self.heapLst[1]
+        self.heapLst[1]=self.heapLst[self.currentSize]
+        self.currentSize=self.currentSize - 1
         self.heapLst.pop()
         self.percUp(1)
         return retval
 
-    def buildHeap(self, lst):
-        i = len(lst) // 2
-        self.currentSize = len(lst)
-        self.heapLst = [0] + lst[:]
-        while (i > 0):
-            self.percDown(i)
-            i = i - 1
